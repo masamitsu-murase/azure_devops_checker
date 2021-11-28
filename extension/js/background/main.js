5d47692b0123a825e5d248b5303263a4d0c292d7
Masamitsu MURASE <masamitsu.murase@gmail.com>
2021-11-29 00:38:19 +0900

    Use alarm.

--- extension/js/background/main.js
+++ extension/js/background/main.js
@@ -1,4 +1,6 @@
 (function () {
+    var gFindMyWorksPromise = null;
+
     var set_badge_text = function (my_pr_count, my_rv_count) {
         if (my_pr_count == 0 && my_rv_count == 0) {
             browser.browserAction.setBadgeText({ text: "" });
@@ -31,32 +33,65 @@
         return await azure_devops.findMyWorks();
     };
 
+    var find_my_works_in_single_request = async function () {
+        if (gFindMyWorksPromise != null) {
+            return await gFindMyWorksPromise;
+        }
+
+        const promise = find_my_works();
+        gFindMyWorksPromise = promise;
+        try {
+            return await promise;
+        } finally {
+            gFindMyWorksPromise = null;
+        }
+    };
+
     var refresh_data = function () {
-        let promise = find_my_works();
-        promise.then(my_works => {
-            const { my_pull_requests, my_review_items } = my_works;
-            set_badge_text(my_pull_requests.length, my_review_items.length);
-        }).catch(_ => {
-            set_badge_error();
-        });
+        (async function () {
+            try {
+                const my_works = await find_my_works_in_single_request();
+                const { my_pull_requests, my_review_items } = my_works;
+                set_badge_text(my_pull_requests.length, my_review_items.length);
+            } catch (e) {
+                set_badge_error();
+            }
+        })();
     };
 
     var initialize = function () {
-        browser.browserAction.setBadgeTextColor({ color: "rgb(255,255,255)" });
+        browser.runtime.onInstalled.addListener(() => {
+            browser.alarms.create("periodic", { periodInMinutes: 1 });
+        });
+
+        if (browser.browserAction.setBadgeTextColor) {
+            browser.browserAction.setBadgeTextColor({ color: "rgb(255,255,255)" });
+        }
         browser.browserAction.setBadgeBackgroundColor({ color: "rgb(255,0,0)" });
+
+        browser.runtime.onMessage.addListener((message, sender) => {
+            switch (message.type) {
+                case "findMyWorks":
+                    return find_my_works_in_single_request();
+                default:
+                    console.error(`Unknown type ${message.type}`);
+                    break;
+            }
+        });
+
+        browser.alarms.onAlarm.addListener(alarm => {
+            switch (alarm.name) {
+                case "periodic":
+                    refresh_data();
+                    break;
+                default:
+                    console.error("Unknown alarm");
+                    break;
+            }
+        })
 
         refresh_data();
     };
-
-    browser.runtime.onMessage.addListener((message, sender) => {
-        switch (message.type) {
-            case "findMyWorks":
-                return find_my_works();
-            default:
-                console.error(`Unknown type ${message.type}`);
-                break;
-        }
-    });
 
     initialize();
 })();
